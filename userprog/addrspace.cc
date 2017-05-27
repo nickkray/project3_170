095ccf8d16f83ae5af6e3fa97bdb2c1377d39344
Nick Ray <nray@umail.ucsb.edu>
2017-05-27 15:44:57 -0700

    from the start

--- userprog/addrspace.cc
+++ userprog/addrspace.cc
@@ -134,6 +134,41 @@ AddrSpace::AddrSpace(OpenFile *executable, PCB* newPCB)
     }
 }
 
+//----------------------------------------------------------------------
+// AddrSpace::AddrSpace
+//     Copy constructor that makes an identical copy of this address space.
+//----------------------------------------------------------------------
+
+AddrSpace::AddrSpace(const AddrSpace* other, PCB* newPCB)
+{
+    // Copy all page table entries over, create associated PCB
+    numPages = other->numPages;
+    DEBUG('a', "Initializing address space with num pages: %d.\n", numPages);
+
+    this->pcb = newPCB;
+    pageTable = new TranslationEntry[numPages];
+
+    for (unsigned int i = 0; i < numPages; i++) { 
+
+        pageTable[i].virtualPage = i;
+        pageTable[i].physicalPage = -1;
+        pageTable[i].valid = FALSE;
+        pageTable[i].use = FALSE;
+        pageTable[i].dirty = FALSE;
+        pageTable[i].readOnly = FALSE;
+
+        // Allocate space for entire addr space on backing store at creation
+        //pageTable[i].space = this;
+        pageTable[i].locationOnDisk = virtualMemoryManager->allocSwapSector();
+        virtualMemoryManager->copySwapSector(pageTable[i].locationOnDisk,
+                                             (other->pageTable)[i].locationOnDisk);
+        
+        // Maintain swap space page information
+        //SwapSectorInfo * swapInfo =
+        //       virtualMemoryManager->getSwapSectorInfo(pageTable[i].locationOnDisk / PageSize);
+        //swapInfo->pageTableEntry = &pageTable[i];
+    }
+}
 
 //----------------------------------------------------------------------
 // AddrSpace::~AddrSpace
@@ -151,7 +186,100 @@ AddrSpace::~AddrSpace()
     }
 }
 
+//----------------------------------------------------------------------
+// AddrSpace::InitRegisters
+// 	Set the initial values for the user-level register set.
+//
+// 	We write these directly into the "machine" registers, so
+//	that we can immediately jump to user code.  Note that these
+//	will be saved/restored into the currentThread->userRegisters
+//	when this thread is context switched out.
+//----------------------------------------------------------------------
 
+void
+AddrSpace::InitRegisters()
+{
+    int i;
+
+    machineLock->Acquire();
+    for (i = 0; i < NumTotalRegs; i++)
+	machine->WriteRegister(i, 0);
+
+    // Initial program counter -- must be location of "Start"
+    machine->WriteRegister(PCReg, 0);	
+
+    // Need to also tell MIPS where next instruction is, because
+    // of branch delay possibility
+    machine->WriteRegister(NextPCReg, 4);
+
+   // Set the stack register to the end of the address space, where we
+   // allocated the stack; but subtract off a bit, to make sure we don't
+   // accidentally reference off the end!
+    machine->WriteRegister(StackReg, numPages * PageSize - 16);
+    machineLock->Release();
+    DEBUG('a', "Initializing stack register to %d\n", numPages * PageSize - 16);
+}
+
+//----------------------------------------------------------------------
+// AddrSpace::SaveState
+// 	On a context switch, save any machine state, specific
+//	to this address space, that needs saving.
+//
+//	For now, nothing!
+//----------------------------------------------------------------------
+
+void AddrSpace::SaveState() 
+{}
+
+//----------------------------------------------------------------------
+// AddrSpace::RestoreState
+// 	On a context switch, restore the machine state so that
+//	this address space can run.
+//
+//      For now, tell the machine where to find the page table.
+//----------------------------------------------------------------------
+
+void AddrSpace::RestoreState() 
+{
+    machine->pageTable = pageTable;
+    machine->pageTableSize = numPages;
+}
+
+//----------------------------------------------------------------------
+// AddrSpace::Translate
+//
+//     Converts a virtual address to a physical address.
+//
+//     Returns -1 for an error in translation, or else the corresponding
+//     physical page number.
+//----------------------------------------------------------------------
+
+int AddrSpace::Translate(int virtualAddress)
+{
+    unsigned int pageTableIndex = (unsigned int) virtualAddress / PageSize;
+    int offset = virtualAddress % PageSize;
+    int physicalAddress = 0;
+
+    if (pageTableIndex >= numPages) {
+        fprintf(stderr,"Page table index: %d bigger than num pages: %d.\n", pageTableIndex, numPages);
+        return -1;
+    } 
+    else if (!pageTable[pageTableIndex].valid) {
+        fprintf(stderr,"Page table index %d is not valid.\n", pageTableIndex);
+        return -1;
+    }
+
+    int frame = pageTable[pageTableIndex].physicalPage;
+
+    if (frame >= NumPhysPages) {
+        fprintf(stderr,"The calculated frame %d larger than num phys pages %d.\n", frame, NumPhysPages);
+        return -1;
+    }
+
+    physicalAddress = frame * PageSize + offset;
+    ASSERT((physicalAddress >= 0));
+    return physicalAddress;
+}
 
 //----------------------------------------------------------------------
 // AddrSpace::ReadFile
@@ -180,3 +308,51 @@ int AddrSpace::ReadFile(int virtAddr, OpenFile* file, int size, int fileAddr)
 
     return numBytesTotal;
 }
+
+//----------------------------------------------------------------------
+// AddrSpace::getPCB
+//     Returns the associated PCB for this address space.
+//----------------------------------------------------------------------
+
+PCB* AddrSpace::getPCB() 
+{
+    return this->pcb;
+}
+
+//----------------------------------------------------------------------
+// AddrSpace::isValid
+//     Checks that we were able to allocate a new address space successfully.
+//----------------------------------------------------------------------
+
+bool AddrSpace::isValid() 
+{
+    return (this->pcb != NULL);
+}
+
+//----------------------------------------------------------------------
+// AddrSpace::getPageTableEntry
+//     Helper function to assist virtual memory in cleanup of physical
+//     pages.
+//----------------------------------------------------------------------
+TranslationEntry* AddrSpace::getPageTableEntry(int pageTableIndex)
+{
+    return &pageTable[pageTableIndex];
+}
+
+//----------------------------------------------------------------------
+// AddrSpace::getPageIndex
+//     Helper function to assist virtual memory in cleanup of physical
+//     pages.
+//----------------------------------------------------------------------
+int AddrSpace::getPageIndex(TranslationEntry* page)
+{
+    for (int i = 0; i < numPages; i++)
+    {
+        if (page == pageTable + i)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
